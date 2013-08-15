edbbacf334f0204a1910b3a70783e1ab7e29c456
Anaz Zubair <anazzubair@yahoo.com>
2013-08-15 05:56:21 +0400

    Added a simple thymeleaf page and an IndexController, shifted to new Glassfish plugin with redeploy support

---
+++ techy/src/main/java/com/anazzubair/techy/web/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.anazzubair.techy.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value="/some.html")
+	public ModelAndView indexPage() {
+		return new ModelAndView("index");
+	}
+	
+	@ModelAttribute("passenger")
+	public String getPassenger(){
+		return "Hana";
+	}
+}
