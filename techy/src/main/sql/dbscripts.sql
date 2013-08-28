7ddbe23db7ef29621c631207227b240c38e14795
Anaz Zubair <anazzubair@yahoo.com>
2013-08-28 22:51:37 +0400

    Added scripts for messageresources table

--- techy/src/main/sql/dbscripts.sql
+++ techy/src/main/sql/dbscripts.sql
@@ -24,3 +24,25 @@ WITH (
 ALTER TABLE users
   OWNER TO anaz;
 
+  
+  
+-- Table: messageresources
+
+DROP TABLE IF EXISTS messageresources;
+
+CREATE TABLE messageresources
+(
+  id bigserial NOT NULL,
+  code text NOT NULL,
+  en text NOT NULL,
+  fr text NOT NULL,
+  CONSTRAINT messageresources_pkey PRIMARY KEY (id),
+  CONSTRAINT messageresources_code_key UNIQUE (code)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE messageresources
+  OWNER TO anaz;
+COMMENT ON TABLE messageresources
+  IS 'I18N Resources for the application';
